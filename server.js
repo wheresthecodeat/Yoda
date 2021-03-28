4c25242790fe9c3d8a78d5adc7b6d0c742fbabef
Glitch (yoda-it-is) <none>
2021-03-28 15:16:27 +0000

    💈🚥 Checkpoint ./server.js:30265742/8:30268209/309

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ client.on("message", message => {
   if (message.content.startsWith(pre + "ping")) {
     message.channel.send("Pong!");
   }
-  else if (message.content.startswith(pre + "prefixes")) {
- message.channel.send("Here are the prefixes for all the droids:" && " @PokéMaster  is ." && "@MasterMyuu is p" && "@PokéDroid is p!" && "@Dank Master is pls" ||  "@PadaGroovy is =" || "@Master Rythm is +" || "@Droid6 is !")
+  else if (message.content(pre + "pres")) {
+ message.channel.send("Here are the prefixes for all the droids: @PokéMaster  is . @MasterMyuu is p  @PokéDroid is p! @Dank Master is pls @PadaGroovy is = @Master Rythm is + @Droid6 is !")
   }
 });
 
-client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.MmtaHRzvkfxzAjfMM4LG_e7dKaQ");
+client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.aTtvwJC18MSdbELAqrvvhk54o5g");
