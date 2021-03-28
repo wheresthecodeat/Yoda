64f65dc8998d75017bc15ca1bd8f6f9f5c3b85ba
Glitch (yoda-it-is) <none>
2021-03-28 14:50:15 +0000

    🍩⛈ Checkpoint ./server.js:30268209/1386

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ const ball = ["Yes", "No"];
 client.on("ready", () => {
   console.log("I am ready!");
 });
-client.on("message", (message) => {
-if (!message.content.startsWith(pre)) return;
+client.on("message", message => {
+  var respond = ball[Math.floor(Math.random() * ball.length)];
+  if (!message.content.startsWith(pre)) return;
   if (message.content.startsWith(pre + "ping")) {
     message.channel.send("Pong!");
   }
-  else if (message.content.startswith(pre + "8ball")){
-var respond = ball[Math.floor(Math.random()*ball.length)]
- message.channel.send(respond).then().catch(console.error);
-}
+  else if (message.content.startswith(pre + "8ball")) {
+    message.channel
+      .send(respond)
+      .then()
+      .catch(console.error);
+  }
 });
- 
+
 client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.MmtaHRzvkfxzAjfMM4LG_e7dKaQ");
