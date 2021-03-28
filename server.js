eadb455596cd076708ef5db4c73f819842a977f2
Glitch (hello-express) <none>
2021-03-28 14:04:39 +0000

    🚎🌦 Checkpoint ./server.js:30268209/1060 ./package.json:30268209/1288

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-let Discord = require("discord.js");
-let client = new Discord.Client();
-
-var ball = ["Yes", "No"];
-
-var response = ball[Math.floor(Math.random() * ball.length)];
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 const pre = "y";
-
-client.on("message", message => {
-  if (message.content === pre + "ping") {
-    message.channel.send("pong");
-  } else if (message.content === pre + "8ball") {
-    message.channel.send(response);
+var ball = ["Yes" , "NO"]
+var response = ball[Math.floor(Math.random()*ball.length)]
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
   }
 });
-
+ 
 client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.x9u5JvGnBKD83vVCZ_ZSrRr5Dds");
