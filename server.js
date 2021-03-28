785794b84accbd7afdb89efb67f48e6f4531e771
Glitch (yoda-it-is) <none>
2021-03-28 16:18:53 +0000

    🐆🎇 Checkpoint ./server.js:30265742/237:30268209/561

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ client.on("message", message => {
     message.channel.send("**Pong!** Ask another bot.");
   } else if (message.content.startsWith(pre + "prefix")) {
     message.channel.send(
-      "Here are the prefixes for all the droids: \n <@716390085896962058> is `.` \n @ is `p` \n PokéRealm is `p!`` \n Dank Memer is `pls` \n @PadaGroovy is = \n @Master Rythm is + \n @Droid6 is !"
+      "Here are the prefixes for all the droids: \n <@716390085896962058> is `.` \n <@438057969251254293> is `p` \n <@665301904791699476> is `p!` \n <@270904126974590976> is `pls` \n <@234395307759108106> is = \n <@235088799074484224> is + \n <@159985870458322944> is !"
     );
+  } if (message.content.startsWith("how do i get admin")) || ((message.content.startsWith("how do i get a higher role")) {
+   message.channel.send(
+   "for now to get an admin ") 
   }
 });
 
