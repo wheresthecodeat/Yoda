3a7c573c8673036b23148b2066878ce38a82fd05
Glitch (yoda-it-is) <none>
2021-03-28 16:07:35 +0000

    🌱💅 Checkpoint ./server.js:30268209/295

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ client.on("ready", () => {
 client.on("message", message => {
   if (!message.content.startsWith(pre)) return;
   if (message.content.startsWith(pre + "ping")) {
-    message.channel.send("**Pong!**");
+    message.channel.send("**Pong!** Ask another bot.");
   } else if (message.content.startsWith(pre + "prefix")) {
     message.channel.send(
-      "Here are the prefixes for all the droids:" /n "@PokéMaster  is ." /n "@MasterMyuu is p" /n  "@PokéDroid is p!" /n "@Dank Master is pls" /n "@PadaGroovy is =" /n  "@Master Rythm is +" || " @Droid6 is !"
+      "Here are the prefixes for all the droids: \n <@716390085896962058> is `.` \n @ is `p` \n PokéRealm is `p!`` \n Dank Memer is `pls` \n @PadaGroovy is = \n @Master Rythm is + \n @Droid6 is !"
     );
   }
 });
