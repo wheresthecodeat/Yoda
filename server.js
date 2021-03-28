d298d20ae442ac9bbcf6ba8f53ae39f27b48d1f2
Glitch (hello-express) <none>
2021-03-28 14:15:57 +0000

    🌻🌄 Checkpoint ./server.js:30268209/1541

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const pre = "y";
-var ball = ["Yes" , "NO"]
-var response = ball[Math.floor(Math.random()*ball.length)]
+var ball = ["Yes", "NO"];
+var response = ball[Math.floor(Math.random() * ball.length)];
 client.on("ready", () => {
   console.log("I am ready!");
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+
+client.on("message", message => {
+  if (message.content(pre + "ping")) {
     message.channel.send("pong!");
+  } else if (message.content.startsWith(pre + "8ball")) {
+    message.channel.send(response);
   }
 });
- 
-client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.x9u5JvGnBKD83vVCZ_ZSrRr5Dds");
+
+client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.FWsBq3Jxt7vp7NyXlmgZqvtj7k4");
