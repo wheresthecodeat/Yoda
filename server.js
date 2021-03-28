0586b64626a2a735f690bfbb6f587db40b7a0e2e
Glitch (yoda-it-is) <none>
2021-03-28 14:27:01 +0000

    🎮😼 Checkpoint ./server.js:30268209/2879 ./package.json:30268209/466

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const pre = "y";
-var ball = ["Yes", "NO"];
-var response = ball[Math.floor(Math.random() * ball.length)];
+var ball = ["Yes" , "NO"]
+var response = ball[Math.floor(Math.random()*ball.length)]
 client.on("ready", () => {
   console.log("I am ready!");
 });
-
-client.on("message", message => {
+ 
+client.on("message", (message) => {
   if (message.content(pre + "ping")) {
     message.channel.send("pong!");
-  } else if (message.content.startsWith(pre + "8ball")) {
-    message.channel.send(response);
   }
+else if (message.content.startsWith(pre + "8ball")){
+message.channel.send(response)
+}
 });
-
+ 
 client.login("ODI1NzAxNjM4NDY2NTY4MjAz.YGBwkw.FWsBq3Jxt7vp7NyXlmgZqvtj7k4");
