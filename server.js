27e631157610d6eccf45c30c10df585d1c19c801
Glitch (yoda-it-is) <none>
2021-03-29 04:55:11 +0000

    🐧🐵 Checkpoint ./server.js:30265742/8189:30268209/84

--- server.js
+++ server.js
@@ -7,23 +7,24 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 client.on("message", message => {
-  if (
-    message.content.startsWith("Hi") ||
-    message.content.startsWith("hi") ||
-    message.content.startsWith("yo") ||
-    message.content.startsWith("Yo")
-  ) {
+  if (message.content.startsWith("Hi") || message.content.startsWith("hi")) {
     message.channel.send(
       "https://tenor.com/view/hello-there-hi-there-greetings-gif-9442662"
     );
-  } else if (message.content.startsWith("oh no") || message.content.startsWith("yikes") || message.content.startsWith("Yikes")) {
+  } else if (
+    message.content.startsWith("oh no") ||
+    message.content.startsWith("yikes") ||
+    message.content.startsWith("Yikes")
+  ) {
     message.channel.send(
       "https://tenor.com/view/yikes-michael-scott-the-office-my-bad-oof-gif-13450971"
     );
   }
   if (!message.content.startsWith(pre)) return;
   if (message.content.startsWith(pre + "ping")) {
-    message.channel.send("**Pong!**").then((sentMessage) => sentMessage.edit("**Pong!** Ask another bot."))
+    message.channel
+      .send("**Pong!**")
+      .then(sentMessage => sentMessage.edit("**Pong!** Ask another bot."));
   } else if (message.content.startsWith(pre + "prefix")) {
     message.channel.send(
       "Here are the prefixes for all the droids: \n <@716390085896962058> is `.` \n <@438057969251254293> is `p` \n <@665301904791699476> is `p!` \n <@270904126974590976> is `pls` \n <@234395307759108106> is = \n <@235088799074484224> is + \n <@159985870458322944> is !"
@@ -40,6 +41,8 @@ client.on("message", message => {
     message.channel.send("<@694494146123268126>");
   } else if (message.content.startsWith(pre + "invite")) {
     message.channel.send("https://discord.gg/GS8PSz4qGD");
+  } else if (message.content.startsWith(pre + "afk")) {
+    message.channel.send("Work in progress.");   
   }
 });
 
