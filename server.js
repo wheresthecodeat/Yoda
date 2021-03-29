3b99722b5317e25e230f86c83d36b3bde6864f9d
Glitch (yoda-it-is) <none>
2021-03-29 04:20:31 +0000

    🌉🍬 Checkpoint ./server.js:30268209/94

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ client.on("message", message => {
   }
   if (!message.content.startsWith(pre)) return;
   if (message.content.startsWith(pre + "ping")) {
-    message.channel.send("**Pong!** Ask another bot.");
+    message.channel.send("**Pong!**");
+    (message.edit("*Pong*!** Ask another bot."))
   } else if (message.content.startsWith(pre + "prefix")) {
     message.channel.send(
       "Here are the prefixes for all the droids: \n <@716390085896962058> is `.` \n <@438057969251254293> is `p` \n <@665301904791699476> is `p!` \n <@270904126974590976> is `pls` \n <@234395307759108106> is = \n <@235088799074484224> is + \n <@159985870458322944> is !"
